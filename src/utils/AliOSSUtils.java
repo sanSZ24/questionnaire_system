3a32833d9a5ca809ce9d2b3c90a46fd32dc04eb1
sanSZ24 <105069750+sanSZ24@users.noreply.github.com>
2023-06-16 16:59:13 +0800

    Create AliOSSUtils.java

---
+++ src/utils/AliOSSUtils.java
@@ -0,0 +1,40 @@
+import org.springframework.web.multipart.MultipartFile;
+import java.io.*;
+import java.utils.UUID;
+
+/**
+ * 阿里云 OSS 工具类
+ */
+@Component
+public class AliOSSUtils {
+
+    String endpoint = "";
+
+    String accessKeyId = "";
+    String accessKeySecret = "";
+
+    String bucketName = "";
+
+    /**
+     * 实现上传图片到OSS
+     */
+    public String upload(MultipartFile file) throws IOException {
+        // 获取上传的文件的输入流
+        InputStream inputStream = file.getInputStream();
+
+        // 避免文件覆盖
+        String originalFilename = file.getOriginalFilename();
+        String fileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //上传文件到 OSS
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        ossClient.putObject(bucketName, fileName, inputStream);
+
+        //文件访问路径
+        String url = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + fileName;
+        // 关闭ossClient
+        ossClient.shutdown();
+        return url;// 把上传到oss的路径返回
+    }
+
+}
